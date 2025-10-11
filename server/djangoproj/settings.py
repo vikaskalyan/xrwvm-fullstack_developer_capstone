8a7f266251e444699360ee1b95ae3903099396c0
vikaskalyan <vikaskalyan1996@gmail.com>
2025-10-10 22:30:53 -0400

    created model

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,10 +29,11 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'localhost','https://vikaskalyan1-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+    'localhost',
+    'https://vikaskalyan1-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
     ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://vikaskalyan1-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+    'https://vikaskalyan1-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
     ]
 
 REST_FRAMEWORK = {
